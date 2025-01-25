a7274af18d261eb43940f77476a4be37196f6734
IsaacVic-Dark <isaacvwarui@gmail.com>
2025-01-25 10:08:29 +0300

    Add show password icon

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -1,37 +1,43 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../services/authService";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
     try {
       const res = await login({ email, password });
       if (res.status === 200) {
         navigate("/welcome");
         alert(" Login successful!");
-      } else if(res.status === 401){
+      } else if (res.status === 401) {
         alert("Invalid credentials");
-      }else{
+      } else {
         alert("Something went wrong please try again");
-        console.log(res?.status)
+        console.log(res?.status);
       }
     } catch (error) {
       console.log("Error in Login", error);
     }
   };
 
-  const isLoginDisabled = email === '' || password.length < 1 || loading;
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const isLoginDisabled = email === "" || password.length < 1 || loading;
 
   return (
     <>
-    <Link to={"/"}>Back</Link>
+      <Link to={"/"}>Back</Link>
       <h1>This is the login page</h1>
       <form>
         <input
@@ -40,14 +46,40 @@ function Login() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
-          type="password"
-          placeholder="Enter Your Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button  onClick={handleLogin} disabled={isLoginDisabled}>
-          {loading ? 'Logging In ...' : 'Login'}
+        <div
+          style={{
+            width: "170px",
+            backgroundColor: "purple",
+            position: "relative",
+          }}
+        >
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Enter Your password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              width: "100%",
+            }}
+          />
+          <i
+            className={showPassword ? "bi bi-eye-fill" : "bi bi-eye-slash-fill"}
+            style={{
+              aspectRatio: "1",
+              height: "100%",
+              position: "absolute",
+              right: "0",
+              top: "50%",
+              transform: "translateY(-39%)",
+              cursor: "pointer",
+            }}
+            onClick={handleShowPassword}
+          />
+        </div>
+
+        <button onClick={handleLogin} disabled={isLoginDisabled}>
+          {loading ? "Logging In ..." : "Login"}
         </button>
       </form>
       <Link to={"/register"}>Create an account</Link>
--- Frontend/src/pages/register.jsx
+++ Frontend/src/pages/register.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom"
 import {register} from '../services/authService'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 function Register() {
   const [ name, setName ] = useState("");
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("");
-  const navigate = useNavigate()
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate()
 
   const handleRegister = async (e) => {
     setLoading(true)
@@ -22,7 +24,11 @@ function Register() {
     } 
   };
 
-  const isRegisterDisabled = name.length < 5 || email === '' || password.length < 1 || loading;
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword)
+  };
+
+  const isRegisterDisabled = name.length < 5 || email === '' || password.length < 6 || loading;
 
   return (
     <>
@@ -31,7 +37,37 @@ function Register() {
       <form>
         <input type="text" placeholder="Enter Your Name" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
         <input type="email" placeholder="Enter Your email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" placeholder="Enter Your password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <div
+          style={{
+            width: "170px",
+            backgroundColor: "purple",
+            position: "relative",
+          }}
+        >
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Enter Your password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              width: "100%",
+            }}
+          />
+          <i
+            className={showPassword ? "bi bi-eye-fill" : "bi bi-eye-slash-fill"}
+            style={{
+              aspectRatio: "1",
+              height: "100%",
+              position: "absolute",
+              right: "0",
+              top: "50%",
+              transform: "translateY(-39%)",
+              cursor: "pointer",
+            }}
+            onClick={handleShowPassword}
+          />
+        </div>
         <button  onClick={handleRegister} disabled={isRegisterDisabled}>
           {loading ? 'Signing Up ...' : 'Register'}
         </button>
